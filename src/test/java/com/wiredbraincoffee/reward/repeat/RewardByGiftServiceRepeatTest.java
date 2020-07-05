88b162bbb0df304a5fe8e106ea9b8e75e18c1477
jabirfayyaz <jabirfayyaz@gmail.com>
2020-07-04 22:02:31 -0500

    added test cases

---
+++ src/test/java/com/wiredbraincoffee/reward/repeat/RewardByGiftServiceRepeatTest.java
@@ -0,0 +1,47 @@
+package com.wiredbraincoffee.reward.repeat;
+
+import com.wiredbraincoffee.reward.RewardByGiftService;
+import com.wiredbraincoffee.reward.RewardInformation;
+import com.wiredbraincoffee.reward.RewardService;
+import org.junit.jupiter.api.*;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/***
+ * @author Jay 
+ *
+ */
+public class RewardByGiftServiceRepeatTest implements TestHelper {
+    private RewardByGiftService reward = null;
+
+    @BeforeEach
+    void setUp() {
+        reward = new RewardByGiftService();
+        reward.setNeededPoints(100);
+        System.out.println("setUp called");
+    }
+
+    @DisplayName("When gift product is not in order, reward should not be applied")
+    @RepeatedTest(value = 3, name =  "-{displayName} -> {currentRepetition}/{totalRepetitions}")
+    void giftProductNotInOrder(RepetitionInfo repetitionInfo) {
+        long productId = repetitionInfo.getCurrentRepetition() + 1000;
+        System.out.println(productId);
+        reward.setGiftProductId(productId);
+
+        RewardInformation info = reward.applyReward(getSampleOrder(), 200);
+
+        assertEquals(0, info.getDiscount());
+    }
+
+    private long getRandomProductIdNotInOrder() {
+        Random r = new Random();
+        return r.longs(1000, 2000).findFirst().getAsLong();
+    }
+
+    @Override
+    public RewardService getRewardService() {
+        return reward;
+    }
+}
